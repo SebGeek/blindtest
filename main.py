2ad4a34be3dacb0d1da92a6e61814cd217f3fe26
sau <sau@lem.com>
2021-12-27 22:30:16 +0100

    up

--- main.py
+++ main.py
@@ -24,12 +24,45 @@ def set_volume(volume):
 
 
 if __name__ == '__main__':
-    # Executed when app is loaded
-    print("init pygame mixer")
-    pygame.mixer.init()
+    pygame.init()
 
-    play_music("music/Musique_Noel.mp3")
+    pygame.joystick.init()
 
-    time.sleep(5)
+    # Count the joysticks the computer has
+    joystick_count = pygame.joystick.get_count()
+    if joystick_count == 0:
+        print("Error, I didn't find any joysticks.")
+    else:
+        # Use joystick #0 and initialize it
+        my_joystick = pygame.joystick.Joystick(0)
+        my_joystick.init()
 
-    stop_music()
+        button_count = my_joystick.get_numbuttons()
+        print(button_count)
+
+        while True:
+            # This gets the position of the axis on the game controller
+            # It returns a number between -1.0 and +1.0
+            # horiz_axis_pos = my_joystick.get_axis(0)
+            # vert_axis_pos = my_joystick.get_axis(1)
+            # print(horiz_axis_pos, vert_axis_pos)
+
+            pygame.event.pump()
+
+            but_list = []
+            for but_item in range(button_count):
+                but_list.append(my_joystick.get_button(but_item))
+            print(but_list)
+
+            time.sleep(0.5)
+
+
+
+    # print("init pygame mixer")
+    # pygame.mixer.init()
+    #
+    # play_music("music/Musique_Noel.mp3")
+    #
+    # time.sleep(5)
+    #
+    # stop_music()
