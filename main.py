d62a97c5b19f8ee562e7fe2260542d8cedb43ca1
sau <sau@lem.com>
2021-12-28 18:55:05 +0100

    up

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 import pygame
-import time
+#import time
+
+# Define some colors.
+BLACK = pygame.Color('black')
+WHITE = pygame.Color('white')
+
+
+# This is a simple class that will help us print to the screen.
+# It has nothing to do with the joysticks, just outputting the
+# information.
+class TextPrint(object):
+    def __init__(self):
+        self.reset()
+        self.font = pygame.font.Font(None, 20)
+
+    def tprint(self, screen, textString):
+        textBitmap = self.font.render(textString, True, BLACK)
+        screen.blit(textBitmap, (self.x, self.y))
+        self.y += self.line_height
+
+    def reset(self):
+        self.x = 10
+        self.y = 10
+        self.line_height = 15
+
+    def indent(self):
+        self.x += 10
+
+    def unindent(self):
+        self.x -= 10
+
 
 
 def play_music(filepath):
@@ -24,45 +54,96 @@ def set_volume(volume):
 
 
 if __name__ == '__main__':
+
     pygame.init()
 
+    print("init pygame sound mixer")
+    pygame.mixer.init()
+
+    play_music("music/Musique_Noel.mp3")
+
+
+    # Set the width and height of the screen (width, height).
+    screen = pygame.display.set_mode((500, 700))
+
+    pygame.display.set_caption("My Game")
+
+    # Loop until the user clicks the close button.
+    done = False
+
+    # Used to manage how fast the screen updates.
+    clock = pygame.time.Clock()
+
+    # Initialize the joysticks.
     pygame.joystick.init()
 
-    # Count the joysticks the computer has
-    joystick_count = pygame.joystick.get_count()
-    if joystick_count == 0:
-        print("Error, I didn't find any joysticks.")
-    else:
-        # Use joystick #0 and initialize it
-        my_joystick = pygame.joystick.Joystick(0)
-        my_joystick.init()
+    # Get ready to print.
+    textPrint = TextPrint()
 
-        button_count = my_joystick.get_numbuttons()
-        print(button_count)
+    # -------- Main Program Loop -----------
+    while not done:
+        #
+        # EVENT PROCESSING STEP
+        #
+        # Possible joystick actions: JOYAXISMOTION, JOYBALLMOTION, JOYBUTTONDOWN,
+        # JOYBUTTONUP, JOYHATMOTION
+        for event in pygame.event.get():  # User did something.
+            if event.type == pygame.QUIT:  # If user clicked close.
+                done = True  # Flag that we are done, so we exit this loop.
+            elif event.type == pygame.JOYBUTTONDOWN:
+                print("Joystick button pressed.")
+            elif event.type == pygame.JOYBUTTONUP:
+                print("Joystick button released.")
 
-        while True:
-            # This gets the position of the axis on the game controller
-            # It returns a number between -1.0 and +1.0
-            # horiz_axis_pos = my_joystick.get_axis(0)
-            # vert_axis_pos = my_joystick.get_axis(1)
-            # print(horiz_axis_pos, vert_axis_pos)
+        #
+        # DRAWING STEP
+        #
+        # First, clear the screen to white. Don't put other drawing commands
+        # above this, or they will be erased with this command.
+        screen.fill(WHITE)
+        textPrint.reset()
 
-            pygame.event.pump()
+        # Get count of joysticks.
+        joystick_count = pygame.joystick.get_count()
 
-            but_list = []
-            for but_item in range(button_count):
-                but_list.append(my_joystick.get_button(but_item))
-            print(but_list)
+        textPrint.tprint(screen, "Number of joysticks: {}".format(joystick_count))
+        textPrint.indent()
 
-            time.sleep(0.5)
+        # For each joystick:
+        for joy_num in range(joystick_count):
+            joystick = pygame.joystick.Joystick(joy_num)
+            joystick.init()
 
+            textPrint.tprint(screen, "Joystick {}".format(joy_num))
+            textPrint.indent()
 
+            # Get the name from the OS for the controller/joystick.
+            name = joystick.get_name()
+            textPrint.tprint(screen, "Joystick name: {}".format(name))
 
-    # print("init pygame mixer")
-    # pygame.mixer.init()
-    #
-    # play_music("music/Musique_Noel.mp3")
-    #
-    # time.sleep(5)
-    #
-    # stop_music()
+            buttons = joystick.get_numbuttons()
+            textPrint.tprint(screen, "Number of buttons: {}".format(buttons))
+            textPrint.indent()
+
+            for _i in range(buttons):
+                button = joystick.get_button(_i)
+                textPrint.tprint(screen,
+                                 "Button {:>2} value: {}".format(_i, button))
+            textPrint.unindent()
+
+        #
+        # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+        #
+
+        # Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+
+        # Limit to 20 frames per second.
+        clock.tick(20)
+
+    stop_music()
+
+    # Close the window and quit.
+    # If you forget this line, the program will 'hang'
+    # on exit if running from IDLE.
+    pygame.quit()
