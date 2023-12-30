2663f4720047d0975a29ab87f7c77f307374d17b
sebastien <famille.auray@gmail.com>
2023-12-30 12:47:49 +0100

    example

---
+++ scratch.py
@@ -0,0 +1,59 @@
+import time
+import pygame
+from gtts import gTTS
+from io import BytesIO
+import glob
+import random
+
+def play_music(filepath):
+    pygame.mixer.music.load(filepath)
+    #print("playing", filepath)
+    try:
+        pygame.mixer.music.play()
+    except pygame.error:
+        print(f"bad format (not MPEG audio): {filepath}")
+
+def play_gun_sound():
+    sounds_guns_list = glob.glob("sounds/guns/*.mp3")
+    play_music(random.choice(sounds_guns_list))
+    time.sleep(2.5)
+
+def pronounce(text_to_pronounce):
+    tts = gTTS(text=text_to_pronounce, lang='fr')
+    mp3 = BytesIO()
+    tts.write_to_fp(mp3)
+    mp3.seek(0)
+    pygame.mixer.music.load(mp3)
+    pygame.mixer.music.play()
+
+def pronounce_start():
+    list_get_ready = ["A vos marques !", "Get ready... Go !", "Attention, c'est parti !"]
+    pronounce(random.choice(list_get_ready))
+    time.sleep(2)
+
+def pronounce_fastest_team(team):
+    play_gun_sound()
+    fastest_team_list = {"blue_team": "Equipe bleue !", "red_team": "Equipe rouge !"}
+    pronounce(fastest_team_list[team])
+    time.sleep(1.5)
+
+def pronounce_score(dict_score):
+    if dict_score["red_team"] > dict_score["blue_team"]:
+        text = f'Score {dict_score["red_team"]} à {dict_score["blue_team"]} pour les rouges !'
+    elif dict_score["red_team"] == dict_score["blue_team"]:
+        text = f'Egalité, {dict_score["red_team"]} partout !'
+    else:
+        text = f'Score {dict_score["blue_team"]} à {dict_score["red_team"]} pour les bleus !'
+    pronounce(text)
+    time.sleep(2.5)
+
+
+if __name__ == '__main__':
+    pygame.mixer.init()
+
+    pronounce_start()
+
+    pronounce_fastest_team("red_team")
+
+    score = {"red_team": 2, "blue_team": 3}
+    pronounce_score(score)
