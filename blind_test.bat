19a18c192a9ae41e33d1f56e612f5a1a7dbeeaf5
sebastien <famille.auray@gmail.com>
2023-12-31 12:06:33 +0100

    bat launcher

--- blind_test.bat
+++
@@ -1,3 +0,0 @@
-rem %USERPROFILE%\AppData\Local\Programs\Python\Python39
-python main.py
-pause
---
+++ quizz_master.bat
@@ -0,0 +1,3 @@
+cd C:\Users\famil\PycharmProjects\blindtest
+C:\Users\famil\PycharmProjects\venv\Scripts\python.exe quizz_master.py
+pause
